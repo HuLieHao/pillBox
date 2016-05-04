6780922b1566dcc7b5b7564b1ab4b0f4539f104c
Huliehao <Maktub714@163.com>
2016-05-05 07:29:52 +0800

    bug f

--- src/main/java/com/pillbox/HelloController.java
+++ src/main/java/com/pillbox/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
